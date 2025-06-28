8078bf97ddab08e1b4b49075b98f833dacb3c140
dachiabashidze <dachi.abashidze@icloud.com>
2025-06-28 18:39:54 +0400

    Add files via upload

---
+++ chat/src/main/java/com/dachiabashidze/chat/entity/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.dachiabashidze.chat.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
